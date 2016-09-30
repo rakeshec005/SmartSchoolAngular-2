1721b9ba085b761b541bb7fadc5aac1dfdf8da15
rakesh <rak1994kumar@gmail.com>
2016-09-30 13:01:00 -0700

    added logo

---
+++ src/app/dashboard.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { dashboardComponent } from './dashboard/dashboard.component';
+
+
+
+
+
+export const APP_ROUTER_PROVIDERS: Routes = [
+  { path: 'dashboard', component: dashboardComponent},
+  
+];
+
