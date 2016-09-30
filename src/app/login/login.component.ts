c68fde0dacf008d70917a41c6ed519cff4e213cc
rakesh <rak1994kumar@gmail.com>
2016-09-30 02:36:19 -0700

    depency installed

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+'use strict';
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl:"login.component.html",
+  styleUrls: ['login.component.css']
+
+})
+export class loginComponent {
+  
+   
+  
+}
---
+++ src/app/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+
+
+
+export const APP_ROUTER_PROVIDERS: Routes = [
+  { path: 'login', component: loginComponent },
+];
+
