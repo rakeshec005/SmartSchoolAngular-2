44851fab661800de930b9497e248d26c8012a1af
rakesh <rak1994kumar@gmail.com>
2016-09-30 21:09:13 -0700

    logout working

--- src/app/header-notification/headerNotification.component.ts
+++ src/app/header-notification/headerNotification.component.ts
@@ -1,14 +1,25 @@
 'use strict';
 import { Component} from '@angular/core';
+import {Router} from '@angular/router'
+import {ToasterModule, ToasterService} from 'angular2-toaster/angular2-toaster';
 
 @Component({
   selector: 'headerNotification',
-  templateUrl:"headerNotification.component.html",
+  templateUrl: "headerNotification.component.html",
   styleUrls: ['headerNotification.component.css']
 
 })
 export class headerNotificationComponent {
-  
-   
-  
+
+  private toasterService: ToasterService;
+
+  constructor(toasterService: ToasterService, private _router: Router) {
+    this.toasterService = toasterService;
+  }
+
+  doLogout() {
+    this._router.navigate(['']);
+    this.toasterService.pop('success', 'Success Logout  !!!!', 'Success Logout !!!!!!');
+  }
+
 }
