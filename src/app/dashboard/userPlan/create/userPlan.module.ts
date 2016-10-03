56f8d4899f1f16901ca15b2c5696faf307e22d0e
rakesh <rak1994kumar@gmail.com>
2016-10-03 12:03:06 -0700

    userPlan

--- src/app/dashboard/userPlan/create/userPlan.module.ts
+++ src/app/dashboard/userPlan/create/userPlan.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { userPlanComponent } from './userPlan.component';
+import { ListUserModule } from '../list/ListUserPlan.module';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [
+        CommonModule, 
+        RouterModule,
+        ListUserModule
+        ],
     declarations: [userPlanComponent],
     exports: [userPlanComponent]
 })
