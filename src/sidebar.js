1321067a25571408034b85dd03981127969db5ab
rakesh <rak1994kumar@gmail.com>
2016-10-02 23:57:35 -0700

    done

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+  var trigger = $('.hamburger'),
+    // overlay = $('.overlay'),
+    isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {
+      trigger.removeClass('fa fa-caret-square-o-right fa-4x');
+      trigger.addClass('fa fa-caret-square-o-left fa-4x');
+      isClosed = false;
+    } else {
+      isClosed = true;
+    }
+
+
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });
 });
