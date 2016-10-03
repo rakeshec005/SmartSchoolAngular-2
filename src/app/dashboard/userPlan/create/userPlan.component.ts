20c9adf535e45a15ddba1ba6f1cddcb046ef3d79
rakesh <rak1994kumar@gmail.com>
2016-10-03 16:00:12 -0700

    userPlan saved done

--- src/app/dashboard/userPlan/create/userPlan.component.ts
+++ src/app/dashboard/userPlan/create/userPlan.component.ts
@@ -1,32 +1,49 @@
 'use strict';
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import {planModel} from '../../../models/plan';
+import {planService} from '../../../services/planService';
 
 @Component({
   selector: 'app-userPlan',
   templateUrl: "userPlan.component.html",
   styleUrls: ['userPlan.component.css'],
-  providers: [],
+  providers: [planService],
 
 })
 export class userPlanComponent implements OnInit {
 
   // variable declaration
   planmodel;
-  
-  constructor() {
-   this.planmodel = new planModel("");
+  successmessages: string;
+  status: Number;
+  success: boolean;
+
+  constructor(private _http: planService) {
+    this.planmodel = new planModel("");
   }
   ngOnInit() {
-  
+
   }
   ngOnDestroy() {
 
   }
 
-saveUserPlan(){
-  console.log("In save UserPlan Page");
-}
+  saveUserPlan() {
+    this._http.createUserPlan(this.planmodel).subscribe(
+      data => {
+        if (data.status == 200) {
+          this.successmessages = data.message;
+          this.status = data.status;
+          this.success = data.success;
+        }
+
+      },
+      error => alert(error),
+      () => console.log("Faild to create UserPlan")
+    );
+
+    console.log("In save UserPlan Page" + this.planmodel.planName);
+  }
 
 
 }
--- src/app/services/planService.ts
+++ src/app/services/planService.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Headers } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class planService {
-    constructor(private _http:Http){}
-    getPlans(){
+    constructor(private _http: Http) { }
+    getPlans() {
         return this._http.get("http://localhost:4000/api/v1/userPlans").
-        map( res=>res.json());
+            map(res => res.json());
+    }
+
+    createUserPlan(planmodel) {
+
+        var headers = new Headers();
+        headers.append("Content-type", "application/json");
+        return this._http.post("http://localhost:4000/api/v1/userPlans/savePlan", {planmodel},{
+                headers: headers
+            }).map(res => res.json());
+
+
     }
 
 }
